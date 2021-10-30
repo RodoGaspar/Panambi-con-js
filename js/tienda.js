9eea94ba3ca0ea5e197af23c4ab3af4ed8c9b212
Rodolfo Gaspar Paredes <gasparianprds@gmail.com>
2021-10-30 12:14:00 -0300

    Avance en js

--- js/tienda.js
+++ js/tienda.js
@@ -1,15 +1,22 @@
-//alert("¡Bienvenido a la tienda de Panambí!")
-var sizePot = prompt("Por favor elige el tipo de maceta que deseas adquirir. Por el momento contamos con tamaños 10, 12, 16 y 18")
+alert("¡Bienvenido a la tienda de Panambí!")
+var sizePot = prompt("Por favor elige el tipo de maceta que deseas adquirir. Por el momento contamos con tamaños 10, 12, 14 y 16")
 var maceta = parseInt(sizePot, 10)
+while (maceta != 10 && maceta != 12 && maceta != 14 && maceta != 16) {
+    maceta = prompt("Por favor, elige en uno de los tamaños disponibles: 10, 12, 14 o 16")
+}
 console.log(maceta)
 var precio = 0
 if (maceta == 10) {
-    precio + 450
+    precio = 450
 } else if (maceta == 12) {
-    precio + 650
+    precio = 650
 } else if (maceta == 14) {
-    precio + 800
+    precio = 800
 } else {
-    precio + 1000
+    precio = 1000
 }
 console.log(precio)
+const iva = x => x * 0.21;
+const suma = (a, b) => a + b;
+let precioFinal = suma(precio, iva(precio))
+console.log (precioFinal)
