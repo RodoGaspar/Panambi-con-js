69acad6de47cf245a27ab859daa380453a914290
Rodolfo Gaspar Paredes <gasparianprds@gmail.com>
2021-11-03 19:29:14 -0300

    desafío clase5 Objetos, algo anda

--- js/tienda.js
+++ js/tienda.js
@@ -1,27 +1,48 @@
 alert("¡Bienvenido a la tienda de Panambí!")
 class producto {
     constructor(nombre, precio) {
-        this.nombre = nombre.toUpperCase();
+        this.nombre = nombre;
         this.precio = parseFloat(precio);
         this.vendido = false;
-    }
+    };
+    precioLista() {
+        if (this.nombre == "S") {
+            this.precio = 450
+        } else if (this.nombre == "M") {
+            this.precio = 650
+        } else if (this.nombre == "L") {
+            this.precio = 800
+        } else {
+            this.precio = 1000
+        } 
+    };
     sumarIva() {
         this.precio = this.precio * 1.21;
-    }
+    };
     vender() {
         this.vendido = true;
-    }
-}
+    };
+};
+const producto1 = new producto (prompt("Por favor elige el tipo de maceta que deseas adquirir. Por el momento contamos con tamaños S, M, L y XL").toUpperCase());
+producto1.precioLista();
+console.log(producto1.nombre);
+console.log(producto1.precio);
+producto1.sumarIva();
+console.log(producto1.precio);
+
+
+
 /*
+var sizePot;
 function size() {
-    let sizePot = prompt("Por favor elige el tipo de maceta que deseas adquirir. Por el momento contamos con tamaños S, M, L y XL").toUpperCase()
+    var sizePot = prompt("Por favor elige el tipo de maceta que deseas adquirir. Por el momento contamos con tamaños S, M, L y XL").toUpperCase();
     while (sizePot != "S" && sizePot != "M" && sizePot != "L" && sizePot != "XL") {
-    sizePot = prompt("Por favor, elige en uno de los tamaños disponibles: S, M, L o XL")
-    return console.log(sizePot)
+    sizePot = prompt("Por favor, elige en uno de los tamaños disponibles: S, M, L o XL");
+    return cosole.log (sizePot)
+    
 }
-console.log(sizePot)
 }
-function precioLista() {
+/*function precioLista() {
     if (sizePot == "S") {
         precio = 450
     } else if (sizePot == "M") {
@@ -32,9 +53,10 @@ function precioLista() {
         precio = 1000
     } 
 }
-precioLista()
-console.log(sizePot)
-console.log(precio)*/
+size()
+//precioLista()
+console.log("El tamaño es " + sizePot)
+//console.log(precio)*/
 
 /*console.log(maceta)
 precioLista(maceta)
